a57b31a7c4b8c61361cd48bdab2d9053419cbca8
jackheuberger <harryhogwarts12345@gmail.com>
2020-08-10 18:25:20 -0400

    Added copy paste directory

--- blitzremover.py
+++ blitzremover.py
@@ -1,11 +1,19 @@
 import os
 
-#Replace PATH_GOES_HERE with the path to your champions directory. Use two backslashes.
-#This is located under league_install/config/Champions
+# Replace PATH_GOES_HERE with the path to your champions directory. Use two backslashes.
+# This is located under league_install/config/Champions
 
-champdirs = 'PATH_GOES_HERE'
-for root, dirs, files in os.walk(champdirs):
+print("Paste the path to your Champions folder.")
+print("EX: C:\Riot Games\League of Legends\Config\Champions")
+path = input()
+champdir = ""
 
+for char in path:
+    if (char == "\\"):
+        champdir += "\\"
+    champdir += char
+
+for root, dirs, files in os.walk(champdir):
     for file_name in files:
         if "@blitz" in file_name:
             print("removing %s..." % root+'\\'+file_name)
